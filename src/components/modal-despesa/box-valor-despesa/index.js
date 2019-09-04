fd9e1b2032d2497e0de7d5487ad1b4933fe4ee86
Marcos Maia <henriquemarcosmaia@gmail.com>
2019-09-04 01:12:02 -0300

    refactor css - valor total conta

--- src/components/modal-despesa/box-valor-despesa/index.js
+++ src/components/modal-despesa/box-valor-despesa/index.js
@@ -1,29 +1,32 @@
 import CriarIcone from "../../shared/criar-icone/index.js";
 import BoxTotalConta from "./box-total-conta/index.js";
+import ImportCss from "../../../utils/import-css/index.js";
+
+ImportCss({ path: 'modal-despesa/box-valor-despesa' });
 
 const BoxTotalDespesa = {
-  build: ({ valorDiscador }) => {
-    const boxTotal = BoxTotalConta.build({ valorDiscador });
-    const excluirTotal = CriarIcone("delete (1).png", "Voltar", "botao-excluir");
+    build: ({ valorDiscador }) => {
+        const boxTotal = BoxTotalConta.build({ valorDiscador });
+        const excluirTotal = CriarIcone("delete (1).png", "Voltar", "botao-excluir");
 
-    boxTotal.appendChild(excluirTotal);
-    excluirTotal.addEventListener("click", clickExcluirTotal);
+        boxTotal.appendChild(excluirTotal);
+        excluirTotal.addEventListener("click", clickExcluirTotal);
 
-    return boxTotal;
-  },
-  remove: () => {
-    const boxTotal = document.querySelector('.box-total')
-    const excluirTotal = document.querySelector('.botao-excluir');
+        return boxTotal;
+    },
+    remove: () => {
+        const boxTotal = document.querySelector('.box-total')
+        const excluirTotal = document.querySelector('.botao-excluir');
 
-    boxTotal && boxTotal.remove();
-    excluirTotal && excluirTotal.removeEventListener('click', clickExcluirTotal);
-  }
+        boxTotal && boxTotal.remove();
+        excluirTotal && excluirTotal.removeEventListener('click', clickExcluirTotal);
+    }
 };
 
 const clickExcluirTotal = () => {
-  const container = document.querySelector(".container");
-  const containerDespesa = document.querySelector(".container-despesa");
-  container.removeChild(containerDespesa);
+    const container = document.querySelector(".container");
+    const containerDespesa = document.querySelector(".container-despesa");
+    container.removeChild(containerDespesa);
 };
 
 
